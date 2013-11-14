b1d3cf39f71c722a8da40ea31fa56e10af4c1adc
macmule <bentoms@btopenworld.com>
2013-11-14 22:01:02 +0000

    Update EAHasWirelessOrAirport.sh

--- EAHasWirelessOrAirport.sh
+++ EAHasWirelessOrAirport.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
+####################################################################################################
+#
+# More information: http://macmule.com/2011/09/08/need-to-find-out-if-a-mac-has-an-airport-card/
+#
+# GitRepo: https://github.com/macmule/EAHasWirelessOrAirport/
+#
+# License: http://macmule.com/license/
+#
+####################################################################################################
 
+# Checks to see if their is a hardware port called AirPort or Wi-Fi
 checkWireless=$(networksetup -listallhardwareports | egrep "Hardware Port: (Air|Wi-)" | cut -c 16-)
+
 if [ -n "${checkWireless}" ]; then
   echo "Yes"
 else
